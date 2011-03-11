7f2304303a7f5675c9ababc01c35f48004d9c868
Hendrik Sollich <hendrik@hoodie.de>
2011-03-11 23:14:42 +0100

    * fixed: taking commands via xmpp now works - now we need some commands

--- jabberbot.rb
+++ jabberbot.rb
@@ -6,8 +6,8 @@ bot = Xxrb.new
 
 # Here we start defining new commands
 
-	do_help = RbCmd.new(:help, :cli)
-	def do_help.action
+	cli_help = RbCmd.new(:help, :cli)
+	def cli_help.action
 		if @args == "me"
 			result = "May I be of some assistence?"
 		else
@@ -15,21 +15,21 @@ bot = Xxrb.new
 		end
 	end
 
-	do_hello = RbCmd.new(:hello, :cli)
-	def do_hello.action
+	cli_hello = RbCmd.new(:hello, :cli)
+	def cli_hello.action
 		@bot.hello
 	end
 
-	do_connect = RbCmd.new(:connect, :cli)
-	def do_connect.action
+	cli_connect = RbCmd.new(:connect, :cli)
+	def cli_connect.action
 		j,p = @args.split(' ',2)
 		@bot.connect(j,p) if @args 
 		@bot.presence_online("I'm on and off for testing")
 		result = " > now we should be connected"
 	end
 
-	do_listen = RbCmd.new(:listen, :cli)
-	def do_listen.action
+	cli_listen = RbCmd.new(:listen, :cli)
+	def cli_listen.action
 		result = @bot.start_xmpp_interface
 	end
  
@@ -38,10 +38,10 @@ bot = Xxrb.new
 # Here we add our commands to the bot
 # Some will later be moved into the Xxrb class and become basic functionality
 
-	bot.add_cmd(do_help)
-	bot.add_cmd(do_hello)
-	bot.add_cmd(do_connect)
-	bot.add_cmd(do_listen)
+	bot.add_cmd(cli_help)
+	bot.add_cmd(cli_hello)
+	bot.add_cmd(cli_connect)
+	bot.add_cmd(cli_listen)
 
 
 bot.start_cli
